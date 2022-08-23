1d615e2b0837ecc7867e1b0e258f5644824ecb70
RyuseiNomi <kanontonto1010@gmail.com>
2022-08-23 21:05:57 +0900

    revise worker process

--- app/workers/sample_event_worker.rb
+++ app/workers/sample_event_worker.rb
@@ -2,7 +2,12 @@ class SampleEventWorker
   include Sidekiq::Worker
   sidekiq_options queue: :event
 
-  def perform
-    puts 'hello world'
+  # 引数同士を加算して logger に出力する
+  #
+  # @param [Integer] a
+  # @param [Integer] b
+  def perform(a, b)
+    result = a + b
+    logger.info("task execution completed! : #{result}")
   end
 end
