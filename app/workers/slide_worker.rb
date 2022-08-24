d2116c3ab23a10f63fcd240f219d03ef64cdf9c3
RyuseiNomi <kanontonto1010@gmail.com>
2022-08-24 21:43:36 +0900

    add slide worker

---
+++ app/workers/slide_worker.rb
@@ -0,0 +1,43 @@
+class SlideWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: :event
+
+  def perform()
+    puts "\n\nお試し！Sidekiq ジョブとテスト記法\n\n"
+    puts "\n\nKanazawa.rb #120\n\n"
+    puts "\n\n-------------------------------------------\n\n"
+    sleep 8
+
+    puts "\n\n\n「とんと」と申します！\n\n普段は Saas デベロッパーをやっています。\n\n\n Ruby / Javascript / Go / QA \n\n\n"
+    puts "\n\n-------------------------------------------\n\n"
+    sleep 8
+
+    puts "\n\nSidekiq って知っていますか？\n\n"
+    puts "\n\n-------------------------------------------\n\n"
+    sleep 3
+
+    puts "\n\nSidekiq はこのように、メインプロセスとは別スレッドで\n非同期的に Ruby のプロセスが実行できるライブラリです。\n\n"
+    puts "\n\n-------------------------------------------\n\n"
+    sleep 8
+
+    puts "\n\n【用意するもの】\n\n"
+    puts "\n\n・Ruby (基本ですね)\n\n"
+    puts "\n\n・Rails (無くても出来ます)\n\n"
+    puts "\n\n・Sidekiq (今回の主役です)\n\n"
+    puts "\n\n・Redis (Sidekiq は基本的に redis とセットで使われます)\n\n"
+    puts "\n\n-------------------------------------------\n\n"
+    sleep 10
+
+    puts "\n\n今回は簡単な足し算をする処理を作ってみました\n\n"
+    puts "\n\n-------------------------------------------\n\n"
+    sleep 5
+
+    puts "\n\n今日は Sidekiq を実演し、その場でテストコードを書いてみようという企画です！\n\n"
+    puts "\n\n-------------------------------------------\n\n"
+    sleep 8
+
+    puts "\n\n対戦よろしくお願いします。\n\n"
+    puts "\n\n-------------------------------------------\n\n"
+    sleep 3
+  end
+end
--- spec/sample_event_worker_spec.rb
+++ spec/sample_event_worker_spec.rb
@@ -3,19 +3,8 @@ require 'sidekiq/testing'
 
 RSpec.describe SampleEventWorker do
 
-  before do
-    Sidekiq::Worker.clear_all
-    allow(Rails.logger).to receive(:info)
-  end
-
   context ' 2 つの引数を受け取った場合' do
     it '足し算をしてログに出力すること' do
-      SampleEventWorker.perform_async(1, 1)
-
-      SampleEventWorker.drain
-
-      expect_result = 2
-      expect(Rails.logger).to have_received(:info).with("result: #{expect_result}")
     end
   end
 end
